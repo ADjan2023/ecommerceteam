8dac93a9251dc3db27d451beb62bb53ea60e89b2
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-11-19 19:41:18 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,14 @@
 <?php
 //Database credentials
-define("SERVER", "localhost");
+/*define("SERVER", "localhost");
 define("USERNAME","root");
 define("PASSWD", "");
+define("DATABASE", "laundrymen");*/
+
+
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME","b1576f5795106b");
+define("PASSWD", "d132953d");
 define("DATABASE", "laundrymen");
 
 
