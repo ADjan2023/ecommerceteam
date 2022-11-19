f30cef038595201e148c8dfcc44dd10b2b866f38
ADjan2023 <banyin.djan@ashesi.edu.gh>
2022-11-19 22:31:43 +0000

    dsfd

--- Admin/viewsubs.php
+++ Admin/viewsubs.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Forms - Admin One Tailwind CSS Admin Dashboard</title>
+  <title>FLaundryMen Admin</title>
 
   <!-- Tailwind is included -->
   <link rel="stylesheet" href="../css/admincss/main.css?v=1628755089081">
