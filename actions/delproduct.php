30c7bff75e06d5c23667bb8efe8bbfcffd0bf9a6
Mike Ofori <102876495+michaelofori@users.noreply.github.com>
2022-11-21 22:00:09 +0000

    n

--- actions/delproduct.php
+++ actions/delproduct.php
@@ -3,9 +3,18 @@
 require("../controllers/product_controller.php");
 
 //collect form data
+<<<<<<< Updated upstream
 if(isset($_POST['delete'])) {
 	$cid=$_POST['pid'];
 	if(del_product_ctr($cid)==true){
+=======
+if (isset($_POST['delete'])) {
+	$id=$_POST['id'];
+	
+	
+	if(delete_product_ctr($id)==true){
+	
+>>>>>>> Stashed changes
 		header('Location:../Admin/viewprod.php');
 	}
 }
